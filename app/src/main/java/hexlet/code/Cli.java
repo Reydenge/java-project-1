720002e5956216bcdcd7c1ee61f51dfe3e956b57
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-19 23:48:52 +0300

    added new class with greeting

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    private String name;
+
+    Cli(String name){
+        this.name = name;
+    }
+
+    public static void getName(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = in.nextLine();
+        System.out.println("Hello, "+name+"!");
+    }
+
+}
