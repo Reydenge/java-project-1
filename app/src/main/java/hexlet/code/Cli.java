7f60dcb7c6b84d8e2a1672b2215bf6392c8ab31c
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-20 11:48:21 +0300

    checksyle plugin remarks fixed

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Cli {
     private String name;
 
-    Cli(String name){
+    Cli(String name) {
         this.name = name;
     }
 
-    public static void getName(){
+    public static void getName() {
         Scanner in = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = in.nextLine();
-        System.out.println("Hello, "+name+"!");
+        System.out.println("Hello, " + name + "!");
     }
 
 }
