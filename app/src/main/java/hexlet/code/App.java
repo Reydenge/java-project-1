1812a9f2a84211304eec080d9e155e50e411336c
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-20 14:36:21 +0300

    added choice of the games

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getName();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter. \n 1 - Greet \n 2 - Even \n 0 - Exit");
+        int gameNumber = in.nextInt();
+        System.out.println("Your choice: " + gameNumber + "\n");
+
+        switch (gameNumber) {
+            case 0:
+                return;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.getName();
+                break;
+            case 2:
+                GamesLogic.evenNumbers();
+                break;
+        }
     }
 }
