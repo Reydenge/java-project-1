960691684e930b040343154f4017144930d0e216
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-21 14:33:00 +0300

    updated second game "Even Numbers"

--- app/src/main/java/hexlet/code/games/GameEven.java
+++ app/src/main/java/hexlet/code/games/GameEven.java
@@ -4,37 +4,15 @@ import hexlet.code.Engine;
 
 import java.util.Scanner;
 
-public class game_even {
+public class GameEven {
     public static void identifyEvenNumbers() {
-        int counter = 0;
         Scanner in = new Scanner(System.in);
         Cli.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        while (counter < 3) {
-            int randomNumber = Engine.getRandomNumber();
-            Engine.question(randomNumber);
-            String playerAnswer = in.nextLine();
-
-            if (randomNumber % 2 == 0 && playerAnswer.equals("yes")) {
-                Engine.correct();
-                counter++;
-            } else if (randomNumber % 2 != 0 && playerAnswer.equals("no")) {
-                Engine.correct();
-                counter++;
-            } else if (randomNumber % 2 == 0 && playerAnswer.equals("no")) {
-                Engine.uncorrectNo();
-                break;
-            } else if (randomNumber % 2 != 0 && playerAnswer.equals("yes")) {
-                Engine.uncorrectYes();
-                break;
-            } else {
-                Engine.uncorrectInput();
-                break;
-            }
-            if (counter == 3) {
-                Engine.congratulation(Cli.getPlayerName());
+        int randomNumber = Engine.getRandomNumber();
+        Engine.question(randomNumber);
+        String rightAnswer = randomNumber%2 == 0 ? "yes" : "no";
+        String playerAnswer = in.nextLine();
+        Engine.checkAnswerWord(playerAnswer, rightAnswer);
             }
         }
-    }
-}
