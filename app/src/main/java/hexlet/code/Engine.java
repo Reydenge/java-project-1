4e9c31217a68851c35735f1a3f62c5db43576598
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-21 17:30:06 +0300

    improved Engine, but checkAnswerWord isnt complete

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,31 +1,63 @@
 package hexlet.code;
 
 public class Engine {
-    public static int getRandomNumber(){
+    public static int winCount = 0;
+    public static int failCount = 0;
+    public static int countForWin = 3;
+    public static int countForLose = 0;
+
+    public static int getRandomNumber() {
         return (int) (Math.random() * (200 - 10 + 1) + 10);
     }
 
-    public static void question(int n){
+    public static void question(int n) {
         System.out.println("Question: " + n);
     }
 
-    public static void congratulation(String name){
+    public static void congratulation(String name) {
         System.out.println("Congratulation, " + name + "!");
     }
 
-    public static void correct(){
+    public static void finishWithWin() {
         System.out.println("Correct!");
     }
 
-    public static void uncorrectYes(){
-        System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again");
+    public static void restart(String name) {
+        System.out.println("Let's try again, " + name + "!");
     }
 
-    public static void uncorrectNo(){
-        System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again");
+    public static void checkAnswerNum(int playerAnswer, int rightAnswer) {
+        if (playerAnswer == rightAnswer) {
+            Engine.finishWithWin();
+            winCount++;
+        } else {
+            System.out.println("'" + playerAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + rightAnswer + "'");
+            failCount++;
+        }
+
+        if (failCount > countForLose) {
+            restart(Cli.getPlayerName());
+        } else if (winCount >= countForWin) {
+            Engine.congratulation(Cli.getPlayerName());
+        }
     }
 
-    public static void uncorrectInput(){
-        System.out.println("Your answer is wrong ;(.\nLet's try again");
+    public static void checkAnswerWord(String playerAnswer, String rightAnswer) {
+
+        if (playerAnswer.equals(rightAnswer)) {
+            Engine.finishWithWin();
+            winCount++;
+        } else {
+            System.out.println("'" + playerAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + rightAnswer + "'");
+            failCount++;
+        }
+
+
+        if (failCount > countForLose) {
+            restart(Cli.getPlayerName());
+        }
+        else if (winCount == countForWin) {
+            Engine.congratulation(Cli.getPlayerName());
+        }
     }
 }
