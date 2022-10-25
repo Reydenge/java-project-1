0b10429587f15e5e36534c9b41f4e09ab6cf8c0e
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-25 11:10:56 +0300

    remark update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,23 +4,34 @@ import hexlet.code.games.GameCalc;
 
 import java.util.Scanner;
 public class App {
+    static final int EXIT = 0;
+    static final int GREETING = 1;
+    static final int GAME_EVEN_NUMBERS = 2;
+    static final int GAME_CALCULATOR = 3;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter. \n 1 - Greet \n 2 - Even \n 0 - Exit");
+        System.out.println("""
+                Please enter the game number and press Enter.
+                 1 - Greet\s
+                 2 - Even\s
+                 3 - Calc\s
+                 0 - Exit""");
         int gameNumber = in.nextInt();
         System.out.println("Your choice: " + gameNumber + "\n");
 
         switch (gameNumber) {
-            case 0:
+            case EXIT:
                 return;
-            case 1:
-                Cli.greeting();
+            case GREETING:
+                Engine.greeting();
                 break;
-            case 2:
+            case GAME_EVEN_NUMBERS:
                 GameEven.identifyEvenNumbers();
                 break;
-            case 3:
+            case GAME_CALCULATOR:
                 GameCalc.calculation();
+                break;
+            default:
         }
     }
 }
