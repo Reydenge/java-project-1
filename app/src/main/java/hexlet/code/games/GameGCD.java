d6d374f4c68fb17ca98d9eee36ec0368d827f137
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-25 16:04:19 +0300

    added GameGCD logic

---
+++ app/src/main/java/hexlet/code/games/GameGCD.java
@@ -0,0 +1,28 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class GameGCD {
+    public static String gameDescription = "Find the greatest common divisor of given numbers.\n";
+
+    private static int gcdOfTwoNumbers (int firstNumber, int secondNumber) {
+        if (secondNumber == 0){
+            return firstNumber;
+        }
+        return gcdOfTwoNumbers(secondNumber, firstNumber % secondNumber);
+    }
+
+    public static void calculateGCD() {
+        int firstNumber;
+        int secondNumber;
+        String[][] gameData = new String[Engine.pointsToWin][2];
+        Engine.greeting();
+        for (int i = 0; i < Engine.pointsToWin; i++) {
+            firstNumber = Engine.getRandomNumber();
+            secondNumber = Engine.getRandomNumber();
+            gameData[i][Engine.dataRightAnswer] = Integer.toString(gcdOfTwoNumbers(firstNumber, secondNumber));
+            gameData[i][Engine.dataPlayerAnswer] = firstNumber + " " + secondNumber;
+            }
+        Engine.checkAnswerWord(gameData, gameDescription);
+        }
+    }
