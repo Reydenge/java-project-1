0b93294bbe0b9a5b086cfe9174b0c391ae4cf298
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-27 13:18:09 +0300

    fix

--- app/src/main/java/hexlet/code/games/GameEven.java
+++ app/src/main/java/hexlet/code/games/GameEven.java
@@ -1,22 +1,21 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
+import hexlet.code.RandomUtils;
 
 public class GameEven {
     public static String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.\n";
+
     private static boolean isEven(int number) {
-        return (number % 2 == 0); }
+        return (number % 2 == 0);
+    }
+
     public static void identifyEvenNumbers() {
-        Engine.greeting();
-        String[][] gameData = new String[Engine.pointsToWin][2];
-        for (int i = 0; i < Engine.pointsToWin; i++) {
-            int randomNumber = Engine.getRandomNumber();
-            gameData[i][Engine.dataPlayerAnswer] = Integer.toString(randomNumber);
-            if (isEven(randomNumber)) {
-                gameData[i][Engine.dataRightAnswer] = "yes";
-            } else {
-                gameData[i][Engine.dataRightAnswer] = "no";
-            }
+        String[][] gameData = new String[Engine.POINTS_TO_WIN][2];
+        for (int i = 0; i < Engine.POINTS_TO_WIN; i++) {
+            int randomNumber = RandomUtils.getRandomNumber();
+            gameData[i][Engine.DATA_QUESTION] = Integer.toString(randomNumber);
+            gameData[i][Engine.DATA_RIGHT_ANSWER] = isEven(randomNumber) ? "yes" : "no";
         }
-        Engine.checkAnswerWord(gameData, gameDescription);
+        Engine.launchAnswerChecking(gameData, gameDescription);
     }
 }
