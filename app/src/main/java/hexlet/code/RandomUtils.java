0a4c609f49c57cb0d5751ecc1ec6d24d14b7d590
Kupchinskii Evgenii <reydenge@DESKTOP-VF14G3B.localdomain>
2022-10-27 14:17:23 +0300

    update

--- app/src/main/java/hexlet/code/RandomUtils.java
+++ app/src/main/java/hexlet/code/RandomUtils.java
@@ -1,10 +1,7 @@
 package hexlet.code;
 
 public class RandomUtils {
-    public static int getRandomNumber() {
-        return (int) (Math.random() * (200 - 10 + 1) + 10);
-    }
-
+    public static final int MAX_NUMBER = 200;
     public static int getLimitedRandomNumber(int limit) {
         return (int) (Math.random() * limit);
     }
