cc294ce396375dbde49807bf3ca74fc3ce9ccb27
Reydenge <kupchinskiievgenii81@gmail.com>
2022-10-27 14:15:27 +0300

    fix

--- app/src/main/java/hexlet/code/games/GameEven.java
+++ app/src/main/java/hexlet/code/games/GameEven.java
@@ -3,8 +3,7 @@ import hexlet.code.Engine;
 import hexlet.code.RandomUtils;
 
 public class GameEven {
-    public static String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.\n";
-
+    private static String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.\n";
     private static boolean isEven(int number) {
         return (number % 2 == 0);
     }
@@ -12,9 +11,9 @@ public class GameEven {
     public static void identifyEvenNumbers() {
         String[][] gameData = new String[Engine.POINTS_TO_WIN][2];
         for (int i = 0; i < Engine.POINTS_TO_WIN; i++) {
-            int randomNumber = RandomUtils.getRandomNumber();
+            int randomNumber = RandomUtils.getLimitedRandomNumber(RandomUtils.MAX_NUMBER);
             gameData[i][Engine.DATA_QUESTION] = Integer.toString(randomNumber);
-            gameData[i][Engine.DATA_RIGHT_ANSWER] = isEven(randomNumber) ? "yes" : "no";
+            gameData[i][Engine.DATA_ANSWER] = isEven(randomNumber) ? "yes" : "no";
         }
         Engine.launchAnswerChecking(gameData, gameDescription);
     }
